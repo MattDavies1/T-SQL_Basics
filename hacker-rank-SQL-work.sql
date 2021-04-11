bce5a61d1d3f73a35c5e7cadd2aec1869e1e74cf
Matt Davies <matt.davies1@mac.com>
2021-04-11 13:07:50 -0400

    Add symmetric Pair solution

--- hacker-rank-SQL-work.sql
+++ hacker-rank-SQL-work.sql
@@ -98,4 +98,16 @@ WHERE names IS NOT NULL
 ORDER BY friend_salary
 ;
 
+-- "Symmetric Pairs"
+-- You are given a table, Functions, containing two columns: X and Y. Two pairs (X1, Y1) and (X2, Y2) are said to be symmetric pairs if X1 = Y2 and X2 = Y1. Write a query to output all such symmetric pairs in ascending order by the value of X. List the rows such that X1 ≤ Y1.
 
+SELECT a.x, a.y
+FROM functions as a
+INNER JOIN
+functions as b
+ON a.x = b.y
+AND a.y = b.x
+GROUP BY a.x, a.y
+HAVING COUNT(*) > 1 OR a.x <a.y
+ORDER BY a.x
+;
